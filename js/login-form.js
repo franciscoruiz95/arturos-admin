207244a127455b47fc8011157b11afca834b1516
Francisco J. P. Ruiz <javierrupe19@gmail.com>
2024-11-21 13:29:32 -0400

    Initial view restored.

--- js/login-form.js
+++ js/login-form.js
@@ -1,29 +1,194 @@
-document.addEventListener('DOMContentLoaded', async function () {
-	const form = document.getElementById('dz_login_signup_form');
-	const data = {
-		"username": form.elements['val-username'].value,
-		"email": form.elements['val-email'].value,
-		"password": form.elements['dz-password'].value
-	}
-
-	form.addEventListener('submit', async function (e) {
-		e.preventDefault();
-		try {
-			const response = await fetch('http://localhost:3000/v1/api/auth/login', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
+var DZFormValidate = function(){
+	
+	var loginForm = function(){
+		
+		jQuery("#dz_login_signup_form").validate({
+				rules: {
+					"val-username": {
+						required: !0,
+						minlength: 3
+					},
+					"val-email": {
+						required: !0,
+						email: !0
+					},
+					"val-password": {
+						required: !0,
+						minlength: 5
+					}
 				},
-				body: JSON.stringify(data)
+				messages: {
+					"val-username": {
+						required: "Please enter a username",
+						minlength: "Your username must consist of at least 3 characters"
+					},
+					"val-email": "Please enter a valid email address",
+					"val-password": {
+						required: "Please provide a password",
+						minlength: "Your password must be at least 5 characters long"
+					}
+				},
+
+				ignore: [],
+				errorClass: "invalid-feedback animated fadeInUp",
+				errorElement: "div",
+				errorPlacement: function(e, a) {
+					jQuery(a).parents(".form-group > div").append(e)
+				},
+				highlight: function(e) {
+					jQuery(e).closest(".form-group").removeClass("is-invalid").addClass("is-invalid");
+				},
+				success: function(e) {
+					jQuery(e).closest(".form-group").removeClass("is-invalid"), jQuery(e).remove()
+				},
+				invalidHandler: function(event, validator) {
+					sweetAlert("Oops...", "Something went wrong !!", "error")
+				},
+				submitHandler: function(form) {
+					sweetAlert("Good...", "Success !!", "success")
+					return false;
+				}
 			});
-			
-			if(response.status === 200) {
-				const responseData = await response.json();
-				localStorage.setItem('token', responseData['token']);
-				window.location.href = 'index.html';
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	});
-}, false);
+
+		
+		jQuery("#dz_login_signin_form").validate({
+				rules: {
+					
+					"val-email": {
+						required: !0,
+						email: !0
+					},
+					"val-password": {
+						required: !0,
+						minlength: 5
+					}
+				},
+				messages: {
+					
+					"val-email": "Please enter a valid email address",
+					"val-password": {
+						required: "Please provide a password",
+						minlength: "Your password must be at least 5 characters long"
+					}
+				},
+
+				ignore: [],
+				errorClass: "invalid-feedback animated fadeInUp",
+				errorElement: "div",
+				errorPlacement: function(e, a) {
+					jQuery(a).parents(".form-group > div").append(e)
+				},
+				highlight: function(e) {
+					jQuery(e).closest(".form-group").removeClass("is-invalid").addClass("is-invalid");
+				},
+				success: function(e) {
+					jQuery(e).closest(".form-group").removeClass("is-invalid"), jQuery(e).remove()
+				},
+				invalidHandler: function(event, validator) {
+					sweetAlert("Oops...", "Something went wrong !!", "error")
+				},
+				submitHandler: function(form) {
+					sweetAlert("Good...", "Success !!", "success")
+					return false;
+				}
+			});
+		
+		jQuery("#dz_login_forgot_form").validate({
+				rules: {
+					"val-password": {
+						required: !0,
+						minlength: 5
+					}
+				},
+				messages: {
+					"val-password": {
+						required: "Please provide a password",
+						minlength: "Your password must be at least 5 characters long"
+					}
+				},
+
+				ignore: [],
+				errorClass: "invalid-feedback animated fadeInUp",
+				errorElement: "div",
+				errorPlacement: function(e, a) {
+					jQuery(a).parents(".form-group > div").append(e)
+				},
+				highlight: function(e) {
+					jQuery(e).closest(".form-group").removeClass("is-invalid").addClass("is-invalid");
+				},
+				success: function(e) {
+					jQuery(e).closest(".form-group").removeClass("is-invalid"), jQuery(e).remove()
+				},
+				invalidHandler: function(event, validator) {
+					sweetAlert("Oops...", "Something went wrong !!", "error")
+				},
+				submitHandler: function(form) {
+					sweetAlert("Good...", "Success !!", "success")
+					return false;
+				}
+			});
+		
+	}
+	
+	
+
+	
+	var formWithIcon = function(){
+		
+		jQuery(".form-valide-with-icon").validate({
+				rules: {
+					"val-username": {
+						required: !0,
+						minlength: 3
+					},
+					"val-password": {
+						required: !0,
+						minlength: 5
+					}
+				},
+				messages: {
+					"val-username": {
+						required: "Please enter a username",
+						minlength: "Your username must consist of at least 3 characters"
+					},
+					"val-password": {
+						required: "Please provide a password",
+						minlength: "Your password must be at least 5 characters long"
+					}
+				},
+
+				ignore: [],
+				errorClass: "invalid-feedback animated fadeInUp",
+				errorElement: "div",
+				errorPlacement: function(e, a) {
+					jQuery(a).parents(".form-group > div").after(e);
+					
+				},
+				highlight: function(e) {
+					jQuery(e).closest(".form-group").removeClass("is-invalid").addClass("is-invalid");
+					
+				},
+				success: function(e) {
+					jQuery(e).closest(".form-group").removeClass("is-invalid").addClass("is-valid")
+					
+				},
+				
+		});
+	}
+	
+	// Public Functions
+    return {
+        // public functions
+        init: function() {
+			loginForm();
+			formWithIcon();
+        }
+    };
+}();
+
+
+// Initialization
+jQuery(document).ready(function() {
+    DZFormValidate.init();
+});
+
