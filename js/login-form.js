1852e73afad538d97e6973d0b987817ceac2c1e4
Francisco J. P. Ruiz <javierrupe19@gmail.com>
2024-11-21 13:23:38 -0400

    First commit.

---
+++ js/login-form.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', async function () {
+	const form = document.getElementById('dz_login_signup_form');
+	const data = {
+		"username": form.elements['val-username'].value,
+		"email": form.elements['val-email'].value,
+		"password": form.elements['dz-password'].value
+	}
+
+	form.addEventListener('submit', async function (e) {
+		e.preventDefault();
+		try {
+			const response = await fetch('http://localhost:3000/v1/api/auth/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(data)
+			});
+			
+			if(response.status === 200) {
+				const responseData = await response.json();
+				localStorage.setItem('token', responseData['token']);
+				window.location.href = 'index.html';
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	});
+}, false);
